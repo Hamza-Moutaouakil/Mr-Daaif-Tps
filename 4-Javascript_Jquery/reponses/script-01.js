92d2afdf4ff14346f69f8c512a57cce244db6c95
Hamza Moutaouakil <45970057+Hamza-Moutaouakil@users.noreply.github.com>
2019-05-19 03:37:22 +0000

    Add files via upload

---
+++ 4-Javascript_Jquery/reponses/script-01.js
@@ -0,0 +1,13 @@
+//  chemin relatif : reponses/script-01.js
+// 
+
+$("#ce div:first").css("color","red");
+
+$("#ce").append($("#ce div:first").clone());
+
+$("#ce").prepend($("#ce div:last"));
+
+$("<div>").appendTo("#ce").text($("#ce div").length);
+
+
+$("#ce div").each((ind, el) => $(el).prepend(ind+" "))
